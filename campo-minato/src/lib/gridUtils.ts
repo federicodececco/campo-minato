3cd4b246000dd622daa4b0ccf66222c17df2c522
federicodececco <federicodececco1@gmail.com>
2025-09-18 17:08:20 +0200

    feat: added classes

---
+++ campo-minato/src/lib/gridUtils.ts
@@ -0,0 +1,85 @@
+enum Grades {
+  easy = "EASY",
+  medium = "MEDIUM",
+  hard = "HARD",
+}
+
+class Grid {
+  sideLength: number;
+  difficulty: Grades;
+  constructor(x: number, difficulty: Grades = Grades.easy) {
+    this.sideLength = x;
+    this.difficulty = difficulty;
+  }
+}
+
+class Casella {
+  bomba: boolean;
+  prossimità: number;
+  constructor(bomba: boolean = false, prossimità: number = 0) {
+    this.bomba = bomba;
+    this.prossimità = prossimità;
+  }
+}
+
+function getRandomInt(min: number, max: number): number {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+/**
+ ** {number} n : lunghezza del lato della board
+ ** {number} bombe: numero di bombe
+ *
+ */
+function generateGrid(board: Grid, bombe?: number): Casella[][] {
+  const n = board.sideLength;
+
+  let grid: Casella[][] = [];
+  let numBombe: number;
+  /* crea un numero di bombe pari al 20% della board o 16, quello più piccolo */
+  if (bombe === undefined) {
+    const maxBombe = Math.floor(n * n * 0.2);
+    numBombe = Math.min(16, maxBombe);
+  } else {
+    numBombe = bombe;
+  }
+  /* riempie la board */
+  for (let i = 0; i < n; i++) {
+    grid[i] = [];
+    for (let j = 0; j < n; j++) {
+      grid[i][j] = new Casella();
+    }
+  }
+  let i = 0;
+  /* posiziona le bombe sulla board */
+  while (i < numBombe) {
+    let x = getRandomInt(0, n);
+    let y = getRandomInt(0, n);
+
+    if (!grid[y][x].bomba) {
+      grid[y][x].bomba = true;
+      i++;
+
+      for (let k = -1; k <= 1; k++) {
+        for (let j = -1; j <= 1; j++) {
+          const newY = y + k;
+          const newX = x + j;
+
+          if (
+            newY >= 0 &&
+            newY < n &&
+            newX >= 0 &&
+            newX < n &&
+            !(k === 0 && j === 0)
+          ) {
+            grid[newY][newX].prossimità++;
+          }
+        }
+      }
+    }
+  }
+
+  return grid;
+}
