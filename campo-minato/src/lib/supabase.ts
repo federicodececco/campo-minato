9a6666e5006a0304745591e6395fe38706a97c28
federicodececco <federicodececco1@gmail.com>
2025-09-21 22:38:25 +0200

    feat: added supabase client

---
+++ campo-minato/src/lib/supabase.ts
@@ -0,0 +1,65 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabseKey);
+
+export class LeaderBoardEntry {
+  name: string;
+  score: number;
+  difficulty: string;
+  stage: number | undefined;
+  time: number;
+  createdAt: string;
+  constructor(
+    name: string,
+    score: number,
+    difficulty: string,
+    time: number,
+    stage?: number
+  ) {
+    this.name = name;
+    this.score = score;
+    this.difficulty = difficulty;
+    this.stage = stage ? stage : undefined;
+    this.time = time;
+    this.createdAt = new Date().getTime.toString();
+  }
+}
+
+export const saveScore = async (
+  leaderBoardData: LeaderBoardEntry
+): Promise<LeaderBoardEntry> => {
+  try {
+    const { data, error } = await supabase
+      .from("leaderboard_entries")
+      .insert([leaderBoardData])
+      .select()
+      .single();
+    if (error) {
+      throw error;
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Errore nell inserimetno in classifica:", error);
+    throw error;
+  }
+};
+
+export const getLeaderBoard = async (maxEntries: number) => {
+  try {
+    const { data, error } = await supabase
+      .from("leaderboard_entries")
+      .select("*")
+      .order("score", { ascending: false })
+      .limit(maxEntries);
+    if (error) {
+      throw error;
+    }
+    return data;
+  } catch (e) {
+    console.error("Errore nel recupero dei dati della classifica:", e);
+    throw e;
+  }
+};
